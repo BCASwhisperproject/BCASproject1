dfc7e82a24093497b1bfce5cc94cfe8d0e686672
Vibhakant <Vibhakant.vaibhav@gmail.com>
2026-04-23 18:33:12 +0530

    Fix cache issue causing blank page

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -57,7 +57,7 @@ export async function GET(req: NextRequest) {
   { posts: formatted, page },
   {
     headers: {
-      "Cache-Control": "private, s-maxage=60, stale-while-revalidate=120",
+      "Cache-Control": "no-store",
     },
   }
 )
